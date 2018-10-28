5d4814621b8269c5306e1939392218274052cfac
YINCHENLI <inchylee831@gmail.com>
2018-10-28 14:41:40 -0700

    added readme

--- src/mapper1.py
+++ src/mapper1.py
@@ -2,6 +2,7 @@ import sys
 import re
 
 total = 0
+
 for line in sys.stdin:
 	line = line.strip()
 	visas = line.split(";")
@@ -9,6 +10,7 @@ for line in sys.stdin:
 	#only count certified visa
 	if visas[2] == 'CERTIFIED':
 		total += 1
+		#clean irrelvant character
 		result = re.sub('[^0-9a-zA-Z,_\\s-]', "", visas[24]) 
 		print('%s\t%s' % (result,1))
 		
--- src/mapper2.py
+++ src/mapper2.py
@@ -1,13 +1,17 @@
 import sys
 import re
+
 total = 0
 
 for line in sys.stdin:
 	line = line.strip()
 	visas = line.split(";")
+	#only count certified visa
 	if visas[2] == 'CERTIFIED':
 		total += 1
+		#remove irrelavant character using pattern
 		result = re.sub('[^a-zA-Z]', "", visas[-3])
 		print('%s\t%s' %(visas[-3], 1))
 
+#print !TOTAL, ! will make sure !TOTAL will comes first after sorting
 print('%s\t%s' %("!TOTAL", total))
